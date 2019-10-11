dfb6037f78bb12b51df0ee9458360dc202941418
Sherukhaeva <sher_var@mail.ru>
2019-10-11 14:57:05 +0400

    new

---
+++ src/test/java/ru/ssau/tk/valyandvitalik/labb/functions/IdentityFunctionTest.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.valyandvitalik.labb.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class IdentityFunctionTest {
+
+    @Test
+    public void testApply() {
+        IdentityFunction identityFunction = new IdentityFunction();
+        for (int j = 0; j < 9; j++) {
+            double z = identityFunction.apply(j);
+            assertEquals(z, j, 0.01);
+        }
+    }
+}
