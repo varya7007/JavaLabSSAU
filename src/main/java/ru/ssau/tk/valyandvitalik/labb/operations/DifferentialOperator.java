71722fd6b0d04083b23842470af152dcc778dbf6
Sherukhaeva <sher_var@mail.ru>
2020-02-18 14:31:35 +0400

    справедливость восстановлена

--- src/main/java/ru/ssau/tk/valyandvitalik/labb/operations/DifferentialOperator.java
+++ src/main/java/ru/ssau/tk/valyandvitalik/labb/operations/DifferentialOperator.java
@@ -2,7 +2,7 @@ package ru.ssau.tk.valyandvitalik.labb.operations;
 
 import ru.ssau.tk.valyandvitalik.labb.functions.MathFunction;
 
-public interface DifferentialOperator<T> extends MathFunction {
+public interface DifferentialOperator<T extends MathFunction> {
     T derive(T function);
 }
 
