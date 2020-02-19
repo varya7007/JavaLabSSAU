5799f5d8d255494e9fa2d50fc5dd83eb091c5fa5
Sherukhaeva <sher_var@mail.ru>
2020-02-19 21:19:33 +0400

    незначительные правки

--- src/main/java/ru/ssau/tk/valyandvitalik/labb/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/valyandvitalik/labb/io/FunctionsIO.java
@@ -23,67 +23,26 @@ public final class FunctionsIO {
         writer.flush();
     }
 
-/*
     public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
         int count;
         try {
             count = Integer.parseInt(reader.readLine());
-        } catch (NumberFormatException e) {
-            throw new IOException(e);
+        } catch (NumberFormatException nfe) {
+            throw new IOException(nfe);
         }
         double[] xValues = new double[count];
         double[] yValues = new double[count];
-        NumberFormat numFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
-        String s;
+        NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        String tempString;
         for (int i = 0; i < count; i++) {
-            s = reader.readLine();
+            tempString = reader.readLine();
             try {
-                xValues[i] = numFormat.parse(s.split(" ")[0]).doubleValue();
-                yValues[i] = numFormat.parse(s.split(" ")[1]).doubleValue();
-            } catch (ParseException p) {
-                throw new IOException(p);
+                xValues[i] = formatter.parse(tempString.split(" ")[0]).doubleValue();
+                yValues[i] = formatter.parse(tempString.split(" ")[1]).doubleValue();
+            } catch (ParseException pe) {
+                throw new IOException(pe);
             }
-            return factory.create(xValues, yValues);
         }
-        return null;
-    }*/
-public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
-    int count;
-    try {
-        count = Integer.parseInt(reader.readLine());
-    } catch (NumberFormatException nfe) {
-        throw new IOException(nfe);
+        return factory.create(xValues, yValues);
     }
-    double[] xValues = new double[count];
-    double[] yValues = new double[count];
-    NumberFormat formatter = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
-    String tempString;
-    for (int i = 0; i < count; i++) {
-        tempString = reader.readLine();
-        try {
-            xValues[i] = formatter.parse(tempString.split(" ")[0]).doubleValue();
-            yValues[i] = formatter.parse(tempString.split(" ")[1]).doubleValue();
-        } catch (ParseException pe) {
-            throw new IOException(pe);
-        }
-    }
-    return factory.create(xValues, yValues);
-}
-
-   /* public static TabulatedFunction readTabulatedFunction(BufferedInputStream inputStream, TabulatedFunctionFactory factory) throws IOException {
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-        int count = dataInputStream.readInt();
-        boolean isStrict = dataInputStream.readBoolean();
-        boolean isUnmodifiable = dataInputStream.readBoolean();
-        double[] xValues = new double[count];
-        double[] yValues = new double[count];
-        for (int i = 0; i < count; i++) {
-            xValues[i] = dataInputStream.readDouble();
-            yValues[i] = dataInputStream.readDouble();
-        }
-        TabulatedFunction function = factory.create(xValues, yValues);
-        function.offerStrict(isStrict);
-        function.offerUnmodifiable(isUnmodifiable);
-        return function;
-    }*/
 }
