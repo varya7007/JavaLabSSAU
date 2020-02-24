a040c6f7473bc43caa5a7a3cf377a9a69536e950
Sherukhaeva <sher_var@mail.ru>
2020-02-24 20:38:34 +0400

    разбивка

--- src/main/java/ru/ssau/tk/valyandvitalik/labb/ui/FormTable.java
+++ src/main/java/ru/ssau/tk/valyandvitalik/labb/ui/FormTable.java
@@ -1,30 +1,33 @@
 package ru.ssau.tk.valyandvitalik.labb.ui;
 
+import ru.ssau.tk.valyandvitalik.labb.exceptions.ArrayIsNotSortedException;
 import ru.ssau.tk.valyandvitalik.labb.functions.TabulatedFunction;
+import ru.ssau.tk.valyandvitalik.labb.functions.factory.LinkedListTabulatedFunctionFactory;
+import ru.ssau.tk.valyandvitalik.labb.functions.factory.TabulatedFunctionFactory;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.table.AbstractTableModel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.table.TableModel;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.lang.Integer.parseInt;
-
-public class Form extends JDialog {
+public class FormTable extends JDialog {
     List<Double> xValues = new ArrayList<>();
     List<Double> yValues = new ArrayList<>();
-    AbstractTableModel tableModel = new MyTableModel(xValues, yValues);
-    //JTable table = new JTable(tableModel);
-    private JButton buttonIn = new JButton("Create table");
-    //private JButton buttonCreate = new JButton("Create");
-    //private JButton buttonView = new JButton("Show table");
+    TableModel tableModel = new MyTableModel(xValues, yValues);
+    JTable table = new JTable(tableModel);
     private JTextField input = new JTextField("", 2);
-    private JLabel label = new JLabel("Input count of point: ");
-    //TabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
+    private JButton buttonCreate = new JButton("Create");
+    TabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
     TabulatedFunction tabulatedFunction;
 
-    public Form() {
+    //private JButton buttonIn = new JButton("Create table");
+    // private JButton buttonView = new JButton("Show table");
+    //private JLabel label = new JLabel("Input count of point: ");
+
+   /* public FormTable() {
         setModal(true);
         setTitle("Input");
         this.setBounds(200, 200, 450, 100);
@@ -34,28 +37,28 @@ public class Form extends JDialog {
         buttonIn.setEnabled(true);
     }
 
-  /*  public Form(Consumer<? super TabulatedFunction> callback) {
+    public FormTable(Consumer<? super TabulatedFunction> callback) {
         setModal(true);
         setTitle("Table Points");
         this.setBounds(300, 300, 500, 500);
         addButtonListener(callback);
         composeInput();
         buttonIn.setEnabled(false);
-        //buttonCreate.setEnabled(false);
+        buttonCreate.setEnabled(false);
     }*/
 
-   /* public Form(TabulatedFunction tabulatedFunction) {
+    public FormTable(TabulatedFunction tabulatedFunction, TableModel tableModel) {
         setTitle("Table Points");
         setModal(true);
         this.tabulatedFunction = tabulatedFunction;
+        this.tableModel = tableModel;
         this.setBounds(100, 100, 300, 500);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         addButtonListener();
         composeTable();
-        buttonView.setEnabled(true);
         buttonCreate.setEnabled(false);
 
-    }*/
+    }
 
     /*public Form(JTable table) {
         setTitle("Table Points");
@@ -68,7 +71,7 @@ public class Form extends JDialog {
         buttonCreate.setEnabled(false);
     }*/
 
-    void composeInput() {
+  /*  void composeInput() {
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setAutoCreateGaps(true);
@@ -83,9 +86,9 @@ public class Form extends JDialog {
                         .addComponent(label)
                         .addComponent(input)
                         .addComponent(buttonIn)));
-    }
+    }*/
 
-   /* void composeTable() {
+    void composeTable() {
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setAutoCreateGaps(true);
@@ -94,40 +97,37 @@ public class Form extends JDialog {
         layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
                 .addGroup(layout.createSequentialGroup())
                 .addComponent(tableScroll)
-                .addComponent(buttonView)
                 .addComponent(buttonCreate));
         layout.setVerticalGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING))
                 .addComponent(tableScroll)
-                .addComponent(buttonView)
                 .addComponent(buttonCreate));
-    }*/
+    }
 
-    /*public void addButtonListener(Consumer<? super TabulatedFunction> callback) {
-        addListenerForButtonIn();
+   /* public void addButtonListener(Consumer<? super TabulatedFunction> callback) {
+        //addListenerForButtonIn();
         //   addListenerForButtonView();
-       *//* addListenerForButtonCreate(callback);
-        addListenerForButton2();*//*
+        addListenerForButtonCreate(callback);
+        addListenerForButton2();
     }*/
 
     public void addButtonListener() {
-        addListenerForButtonIn();
-        //  addListenerForButtonView();
-       /* addListenerForButtonCreate();
-        addListenerForButton2();*/
+        addListenerForButtonCreate(tableModel);
+        addListenerForButton2();
     }
 
-    public void clearTable(int n) {
+  /*  public void clearTable(int n) {
         for (int i = n; i > 3; i--) {
             xValues.remove(i - n + 1);
             yValues.remove(i - n + 1);
             tableModel.fireTableDataChanged();
         }
-    }
+    }*/
 
-    public void addListenerForButtonIn() {
+   /* public void addListenerForButtonIn() {
         buttonIn.addActionListener(event -> {
             try {
+                buttonCreate.setEnabled(false);
                 int count = parseInt(input.getText());
                 clearTable(tableModel.getColumnCount());
                 for (int i = 0; i < count; i++) {
@@ -135,14 +135,15 @@ public class Form extends JDialog {
                     yValues.add(0.);
                     tableModel.fireTableDataChanged();
                 }
-                buttonIn.addActionListener(new ButtonEventListener(count));
-               // tableModel.getRowCount();
+                if (tableModel.getRowCount() > 1) {
+                    buttonCreate.setEnabled(true);
+                }
             } catch (Exception e) {
                 new ErrorWindow(this, e);
+
             }
         });
-
-    }
+    }*/
 
     /*public void addListenerForButtonView() {
         buttonView.addActionListener(event -> {
@@ -165,7 +166,11 @@ public class Form extends JDialog {
         });
     }*/
 
-   /* public void addListenerForButtonCreate() {
+    public void addListenerForButtonCreate(TableModel tableModel) {
+        this.tableModel = tableModel;
+        if (this.tableModel.getRowCount() > 1) {
+            buttonCreate.setEnabled(true);
+        }
         buttonCreate.addActionListener(event -> {
             try {
                 double[] x = new double[xValues.size()];
@@ -186,7 +191,7 @@ public class Form extends JDialog {
         });
     }
 
-    public void addListenerForButtonCreate(Consumer<? super TabulatedFunction> callback) {
+   /* public void addListenerForButtonCreate(Consumer<? super TabulatedFunction> callback) {
         buttonCreate.addActionListener(event -> {
             try {
                 double[] x = new double[xValues.size()];
@@ -208,7 +213,7 @@ public class Form extends JDialog {
         });
     }*/
 
-   /* public void addListenerForButton2() {
+    public void addListenerForButton2() {
         input.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
@@ -225,35 +230,18 @@ public class Form extends JDialog {
                 onChanged();
             }
         });
-    }*/
+    }
 
-   /* private void onChanged() {
+    private void onChanged() {
         buttonCreate.setEnabled(!input.getText().isEmpty());
-    }*/
+    }
 
-    class ButtonEventListener implements ActionListener {
-        ButtonEventListener(int count){
-
-        }
+    /*class ButtonEventListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            FormTable app = new FormTable(tabulatedFunction, tableModel);
+            FormTable app = new FormTable(tabulatedFunction);
             app.setVisible(true);
         }
-    }
 
-    public static void main(String[] args) {
-        Form app = new Form();
-        app.setVisible((true));
-    }
-
-   /* public static void main(TabulatedFunction tabulatedFunction) {
-          FormTable app = new FormTable(tabulatedFunction);
-          app.setVisible(true);
-      }
-
-    public static void main(Consumer<? super TabulatedFunction> callback) {
-        Form app = new Form(callback);
-        app.setVisible(true);
     }*/
 }
